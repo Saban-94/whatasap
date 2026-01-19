1dc2e242602259e6d9f061b7762939fefdc31759
Saban-94 <hsaban2025@gmail.com>
2026-01-19 02:14:35 +0200

    Update page.tsx

--- src/app/client/[id]/page.tsx
+++ src/app/client/[id]/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { db } from "../../../firebase";
+import { db } from "@/lib/firebase";
 import { collection, query, where, onSnapshot } from "firebase/firestore";
 import { useState, useEffect } from 'react';
 
