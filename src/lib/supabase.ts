21c40b906d2225cecf0f815bf3ba635499fa3cbd
Saban-94 <hsaban2025@gmail.com>
2026-01-19 02:59:23 +0200

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
-// יצירת הלקוח להתחברות למאגר
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase: any = createClient(supabaseUrl, supabaseAnonKey);
