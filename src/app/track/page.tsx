4c9a9e02b1cef336f9c2a8d86215d1d27f6939af
Saban-94 <hsaban2025@gmail.com>
2026-01-19 03:45:03 +0200

    Update page.tsx

--- src/app/track/page.tsx
+++ src/app/track/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 export const dynamic = 'force-dynamic';
-import { db } from "../firebase";
+import { db } from "@/lib/firebase"
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { useState } from 'react';
 import Link from 'next/link';
