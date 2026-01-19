61f2e34284b3c73524baf1899debbf3245d8506e
Saban-94 <hsaban2025@gmail.com>
2026-01-19 02:15:35 +0200

    Update page.tsx

--- src/app/admin/studio/page.tsx
+++ src/app/admin/studio/page.tsx
@@ -1,6 +1,6 @@
 'use client';
-import { db } from "../../../firebase";
-import { collection, addDoc, getDocs, doc, deleteDoc, query, orderBy, limit } from "firebase/firestore";
+src/app/admin/studio/page.tsx
+  import { collection, addDoc, getDocs, doc, deleteDoc, query, orderBy, limit } from "firebase/firestore";
 import { useState, useEffect } from 'react';
 
 export default function AdminStudio() {
