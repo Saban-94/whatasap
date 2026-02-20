4c611a22952ded60771c2bf7e31a578b05cf5dce
Saban-94 <hsaban2025@gmail.com>
2026-02-20 09:15:01 +0200

    Update page.tsx

--- src/app/shahar/page.tsx
+++ src/app/shahar/page.tsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import { Send, Paperclip, Smile, MoreVertical, ShoppingCart, Package, Plus, Bell, ShieldCheck, CreditCard } from 'lucide-react';
 import OrderSidebar from './components/OrderSidebar';
 
-const SHAHAR_PROFILE_PIC = "https://api.dicebear.com/7.x/pixel-art/svg?seed=ShaharShaul";
+const SHAHAR_PROFILE_PIC = "https://media-mrs2-1.cdn.whatsapp.net/v/t61.24694-24/524989315_1073256511118475_7315275522833323073_n.jpg?ccb=11-4&oh=01_Q5Aa3wFxRPXggH-pzRFes-D1aIk6klzJrTv9Ks5RbOrhtvKfvQ&oe=69A5059E&_nc_sid=5e03e0&_nc_cat=111";
 const SABAN_AI_PROFILE_PIC = "https://api.dicebear.com/7.x/bottts/svg?seed=SabanAI";
 
 export default function ShaharChatPage() {
